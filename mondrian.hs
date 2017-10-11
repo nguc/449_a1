8eb6cce059667316a4e5057b17bfa63c62df2faf
nguc <c.ngu.91@gmail.com>
2017-10-10 20:25:24 -0600

    Getting started
    Don't know what i'm doing

--- mondrian.hs
+++ mondrian.hs
@@ -60,6 +60,53 @@ mondrian x y w h (r:s:t:rs) =
                          (show (round (s * 255))) ++ "," ++
                          (show (round (t * 255))) ++ ")\" />\n")
 
+
+------------------------Added code ------------------------------------
+
+  -- Divide a region 
+  -- If region is wider and taller than half the initial canvas size, split into 4 smaller regions
+
+  -- Parameters:
+  --   x, y
+  --   w, h the width and height of the region
+  --   (r:s:rs) a list of random Int numbers to generate the split points
+  -- Return: 
+  --   A string containing the html tags and a list of the unused random numbers
+  --   
+
+-- just split vertical
+split :: Int -> Int -> Int -> Int -> [Float] -> (String, [Float])
+split x y w h (r:s:rs) = mondrian (randX 0 w h rs)
+
+-- take a random float, x, w Return a random split position in the x axis
+randX :: Int -> Int -> Float -> Float
+randX x w r
+  | r > x && r < (w * 1.5) = r
+  | otherwise = 0
+
+  --split x y w h (r:s:rs)
+  -- | w >= 120 && h >= 120 = divideInFour( )
+  -- | otherwise = splitVertical x y w r
+
+
+--divideInFour :: 
+
+--splitVertical :: Int -> Int -> Float
+-- return a random number between (.33 * w) and (.67 * w)
+
+--splitHorizontal :: Int -> Int -> Float
+-- return a random number between (.33 * h) and (.67 * h)
+
+
+
+
+
+
+
+
+
+
+
 --
 -- The main program which generates and outputs mondrian.html.
 --
@@ -80,3 +127,5 @@ main = do
       suffix = "</svg>\n</html>"
 
   writeFile "mondrian.html" (prefix ++ image ++ suffix)
+
+  
